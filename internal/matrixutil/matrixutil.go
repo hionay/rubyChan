bf8f0cd4d9c2e03e0027229580d5ef84d2b5e804
Halil ibrahim Onay <hionay@gmail.com>
2026-03-18 17:44:41 +0300

    refactor: move common funcs to matrixutil

---
+++ internal/matrixutil/matrixutil.go
@@ -0,0 +1,54 @@
+package matrixutil
+
+import (
+	"context"
+	"fmt"
+	"html"
+	"sort"
+	"strings"
+
+	"maunium.net/go/mautrix"
+	"maunium.net/go/mautrix/id"
+)
+
+type KV struct {
+	K string
+	V int
+}
+
+func TopN(m map[string]int, n int) []KV {
+	out := make([]KV, 0, len(m))
+	for k, v := range m {
+		out = append(out, KV{K: k, V: v})
+	}
+	sort.Slice(out, func(i, j int) bool {
+		if out[i].V == out[j].V {
+			return out[i].K < out[j].K
+		}
+		return out[i].V > out[j].V
+	})
+	if len(out) > n {
+		out = out[:n]
+	}
+	return out
+}
+
+func DisplayNick(ctx context.Context, cli *mautrix.Client, roomID id.RoomID, mxid string) string {
+	if cli.StateStore != nil {
+		if member, err := cli.StateStore.GetMember(ctx, roomID, id.UserID(mxid)); err == nil && member != nil {
+			if member.Displayname != "" {
+				return member.Displayname
+			}
+		}
+	}
+	if i := strings.IndexByte(mxid, ':'); i > 1 && mxid[0] == '@' {
+		return mxid[1:i]
+	}
+	return mxid
+}
+
+func MentionNickHTML(ctx context.Context, cli *mautrix.Client, roomID id.RoomID, mxid string) string {
+	nick := DisplayNick(ctx, cli, roomID, mxid)
+	return fmt.Sprintf(`<a href="https://matrix.to/#/%s">@%s</a>`,
+		html.EscapeString(mxid), html.EscapeString(nick))
+}
